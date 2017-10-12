4134d2fb49c99d2a2b6f184126f6301fa9802c2f
GeorgeKihara <gegohcomp@gmail.com>
2017-10-12 19:46:19 +0300

    html pages

--- app/views.py
+++ app/views.py
@@ -5,14 +5,16 @@ from werkzeug import secure_filename
 import bcrypt, json, requests, bson.binary
 
 #connections to the mongo database
-#app.config['MONGO_DBNAME'] = ''
-#app.config['MONGO_URI'] = ''
+app.config['MONGO_DBNAME'] = 'mongotry'
+app.config['MONGO_URI'] = 'mongodb://admin:admin@ds111535.mlab.com:11535/mongotry'
 
-#db = PyMongo(app)
+db = PyMongo(app)
 
 #index page
 @app.route('/')
 def index():
+    if 'name' in session:
+    	return 'you are logged in as ' + session['name']
     return render_template('index.html')
 
 
